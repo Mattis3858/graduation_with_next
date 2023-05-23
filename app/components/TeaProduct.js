5a802029fd6471f36440a302cd353e10e3005cd5
Mattis <mattis3858@yahoo.com.tw>
2023-05-23 17:09:52 +0800

    更新茶產品Component

--- app/components/TeaProduct.js
+++ app/components/TeaProduct.js
@@ -13,10 +13,10 @@ function TeaProduct({ src, name = '', description = '', price = 0 }) {
         <div className="mt-2 ml-3 mb-2 text-xl font-semibold">${price}</div>
       </div>
       <div className="flex justify-evenly ">
-        <button className="h-8 bg-gray-300 mb-2 rounded-2xl w-24 border-lime-600 border-2">
+        <button className="h-10 bg-lime-600 mb-4 rounded-2xl w-28 text-2xl text-white font-semibold">
           預約品茶
         </button>
-        <button className="h-8 bg-gray-300 mb-2 rounded-2xl w-24 border-lime-700 border-2">
+        <button className="h-10 bg-lime-600 mb-4 rounded-2xl w-28 text-2xl text-white font-semibold">
           購買
         </button>
       </div>
