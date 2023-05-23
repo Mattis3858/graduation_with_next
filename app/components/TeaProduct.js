d2769659c5c1a8d4525dfb9194762959db5c46ca
Mattis3858 <109306011@nccu.edu.tw>
2023-05-23 16:53:36 +0800

    更新木柵茶本舖頁面

--- app/components/TeaProduct.js
+++ app/components/TeaProduct.js
@@ -2,14 +2,23 @@ import React from 'react';
 
 function TeaProduct({ src, name = '', description = '', price = 0 }) {
   return (
-    <div className="border-solid border-2 border-gray-400 rounded-2xl mt-5 bg-gray-200 ">
+    <div className="border-solid border-2 border-lime-600 rounded-2xl mt-5 bg-gray-200 font-sans">
       <div className="w-full">
-        <img src={src} alt="Tea" width="250px" height="250px" />
+        <img src={src} alt="Tea" className="block mx-auto w-1/2" />
       </div>
+      <hr className="h-0.5 bg-lime-600 border-0" />
       <div>
-        <div className="mt-4">{name}</div>
-        <div className="mt-4">{description}</div>
-        <div className="mt-4">${price}</div>
+        <div className="mt-2 ml-3 text-2xl font-bold">{name}</div>
+        <div className="mt-2 ml-3">{description}</div>
+        <div className="mt-2 ml-3 mb-2 text-xl font-semibold">${price}</div>
+      </div>
+      <div className="flex justify-evenly ">
+        <button className="h-8 bg-gray-300 mb-2 rounded-2xl w-24 border-lime-600 border-2">
+          預約品茶
+        </button>
+        <button className="h-8 bg-gray-300 mb-2 rounded-2xl w-24 border-lime-700 border-2">
+          購買
+        </button>
       </div>
     </div>
   );
--- app/product/Product.js
+++ app/product/Product.js
@@ -4,24 +4,42 @@ import TeaProduct from '../components/TeaProduct';
 const Product = () => {
   return (
     <div>
-      <div className="font-bold text-4xl mt-5">木柵茶本舖</div>
-      <div className="flex justify-around">
+      <div className="font-bold text-4xl mt-5 mb-5">木柵茶本舖</div>
+      <div className="grid justify-around gap-x-12 gap-y-5 grid-cols-3 ml-10 mr-10 mb-10">
         <TeaProduct
           src="images/green-tea.png"
           name="Green Tea"
-          description="This is a cup of green tea"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus quam sem, vel euismod metus elementum quis. Proin libero purus, feugiat non ligula et, mattis congue ante. Proin posuere lacinia vulputate. Sed efficitur pretium tincidunt. Morbi aliquam nisi ex, ac consectetur ipsum auctor eget. "
           price={555}
         />
         <TeaProduct
           src="images/green-tea.png"
           name="Green Tea"
-          description="This is a cup of green tea"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus quam sem, vel euismod metus elementum quis. Proin libero purus, feugiat non ligula et, mattis congue ante. Proin posuere lacinia vulputate. Sed efficitur pretium tincidunt. Morbi aliquam nisi ex, ac consectetur ipsum auctor eget. "
           price={555}
         />
         <TeaProduct
           src="images/green-tea.png"
           name="Green Tea"
-          description="This is a cup of green tea"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus quam sem, vel euismod metus elementum quis. Proin libero purus, feugiat non ligula et, mattis congue ante. Proin posuere lacinia vulputate. Sed efficitur pretium tincidunt. Morbi aliquam nisi ex, ac consectetur ipsum auctor eget. "
+          price={555}
+        />
+        <TeaProduct
+          src="images/green-tea.png"
+          name="Green Tea"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus quam sem, vel euismod metus elementum quis. Proin libero purus, feugiat non ligula et, mattis congue ante. Proin posuere lacinia vulputate. Sed efficitur pretium tincidunt. Morbi aliquam nisi ex, ac consectetur ipsum auctor eget. "
+          price={555}
+        />
+        <TeaProduct
+          src="images/green-tea.png"
+          name="Green Tea"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus quam sem, vel euismod metus elementum quis. Proin libero purus, feugiat non ligula et, mattis congue ante. Proin posuere lacinia vulputate. Sed efficitur pretium tincidunt. Morbi aliquam nisi ex, ac consectetur ipsum auctor eget. "
+          price={555}
+        />
+        <TeaProduct
+          src="images/green-tea.png"
+          name="Green Tea"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras rhoncus quam sem, vel euismod metus elementum quis. Proin libero purus, feugiat non ligula et, mattis congue ante. Proin posuere lacinia vulputate. Sed efficitur pretium tincidunt. Morbi aliquam nisi ex, ac consectetur ipsum auctor eget. "
           price={555}
         />
       </div>
