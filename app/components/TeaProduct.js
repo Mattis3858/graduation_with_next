7bb6c6d8fc77b3e12e41e11b5dab4a7aaac41cd7
Mattis3858 <109306011@nccu.edu.tw>
2023-05-23 19:20:12 +0800

    更新木柵茶本舖頁面

--- app/components/TeaProduct.js
+++ app/components/TeaProduct.js
@@ -10,13 +10,13 @@ function TeaProduct({ src, name = '', description = '', price = 0 }) {
       <div>
         <div className="mt-2 ml-3 text-2xl font-bold">{name}</div>
         <div className="mt-2 ml-3">{description}</div>
-        <div className="mt-2 ml-3 mb-2 text-xl font-semibold">${price}</div>
+        <div className="mt-2 ml-3 mb-2 text-2xl font-semibold ">${price}</div>
       </div>
       <div className="flex justify-evenly ">
-        <button className="h-10 bg-lime-600 mb-4 rounded-2xl w-28 text-2xl text-white font-semibold">
+        <button className="h-10 bg-lime-600 mb-4 rounded-2xl w-28 text-2xl text-white font-semibold hover:bg-lime-400">
           預約品茶
         </button>
-        <button className="h-10 bg-lime-600 mb-4 rounded-2xl w-28 text-2xl text-white font-semibold">
+        <button className="h-10 bg-lime-600 mb-4 rounded-2xl w-28 text-2xl text-white font-semibold hover:bg-lime-400">
           購買
         </button>
       </div>
