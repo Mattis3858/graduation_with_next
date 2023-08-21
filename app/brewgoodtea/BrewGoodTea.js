44ba1d5b02c6dff21af82e502bdbcb7104bf8fd5
bugee1234 <bugee1234@gmail.com>
2023-08-21 14:56:31 +0800

    brewgoodtea backend model connection

--- app/brewgoodtea/BrewGoodTea.js
+++ app/brewgoodtea/BrewGoodTea.js
@@ -1,20 +1,30 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 
 const BrewGoodTea = () => {
-  const [file, setFile] = useState();
+  const [file, setFile] = useState(null);
   const [message, setMessage] = useState('');
-  function handleChange(e) {
-    console.log(e.target.files);
-    setFile(URL.createObjectURL(e.target.files[0]));
-  }
-  useEffect(() => {
-    fetch('http://127.0.0.1:8080/api/home')
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
+
+  async function handleChange(e) {
+    const selectedFile = e.target.files[0];
+    setFile(selectedFile);
+    
+    const formData = new FormData();
+    formData.append('file', selectedFile);
+
+    try {
+      const response = await fetch('http://127.0.0.1:8080/api/home', {
+        method: 'POST',
+        body: formData,
       });
-  }, []);
+
+      const data = await response.json();
+      setMessage(`相似程度: ${data.similarity}`);
+    } catch (error) {
+      console.error('Error uploading file:', error);
+    }
+  }
+
   return (
     <div className="ml-10">
       <div className="font-bold text-4xl mt-6 text-center">泡好茶</div>
@@ -26,55 +36,9 @@ const BrewGoodTea = () => {
         accept="image/gif, image/jpeg, image/png"
       />
       <div>{message}</div>
-      <img src={file} />
+      {file && <img src={URL.createObjectURL(file)} alt="Selected" />}
     </div>
   );
 };
 
 export default BrewGoodTea;
-// 'use client';
-// import React, { useState, useRef } from 'react';
-
-// const BrewGoodTea = () => {
-//   const [file, setFile] = useState();
-//   const [cameraStream, setCameraStream] = useState();
-//   const videoRef = useRef();
-
-//   function handleChange(e) {
-//     console.log(e.target.files);
-//     setFile(URL.createObjectURL(e.target.files[0]));
-//   }
-
-//   async function openCamera() {
-//     try {
-//       const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-//       setCameraStream(stream);
-//       if (videoRef.current) {
-//         videoRef.current.srcObject = stream;
-//       }
-//     } catch (error) {
-//       console.error('Error accessing camera:', error);
-//     }
-//   }
-
-//   return (
-//     <div className="ml-10">
-//       <div className="font-bold text-4xl mt-6 text-center">泡好茶</div>
-//       <h2>Add Image:</h2>
-//       <input
-//         id="imgTea"
-//         type="file"
-//         onChange={handleChange}
-//         accept="image/gif, image/jpeg, image/png"
-//       />
-//       <br />
-//       <button onClick={openCamera}>Open Camera</button>
-//       {cameraStream && (
-//         <video ref={videoRef} autoPlay style={{ objectFit: 'cover' }} />
-//       )}
-//       {file && <img src={file} />}
-//     </div>
-//   );
-// };
-
-// export default BrewGoodTea;
