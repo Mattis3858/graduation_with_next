655337da11c60b8cbb73643397feaa6a9c6ded0a
Mattis3858 <109306011@nccu.edu.tw>
2023-08-25 15:37:02 +0800

    新增登入功能

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,52 @@
+import type { NextAuthOptions } from 'next-auth';
+import GitHubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const options: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID as string,
+      clientSecret: process.env.GOOGLE_SECRET as string,
+      authorization: {
+        params: {
+          prompt: 'consent',
+          access_type: 'offline',
+          response_type: 'code',
+        },
+      },
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: {
+          label: 'Username:',
+          type: 'text',
+          placeholder: 'WCDATS',
+          value: 'WCDATS',
+        },
+        password: {
+          label: 'Password:',
+          type: 'password',
+          placeholder: 'WCDATS',
+          value: 'WCDATS',
+        },
+      },
+      async authorize(credentials) {
+        const user = { id: '', name: 'WCDATS', password: 'WCDATS' };
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+};
