76341632a3cec0acd678e6607e7bd828e71df216
Mattis3858 <109306011@nccu.edu.tw>
2023-09-20 16:45:19 +0800

    更新網址

--- app/personalInformation/page.js
+++ app/personalInformation/page.js
@@ -1,6 +1,24 @@
+'use client';
 import Profile from './Profile';
+import { useEffect, useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
+const supabase = createClient(
+  'https://zdxlzmekrckaffbzupmh.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpkeGx6bWVrcmNrYWZmYnp1cG1oIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODgzNzM1MTEsImV4cCI6MjAwMzk0OTUxMX0.YI14GVJfa6H0eXOUqCKXT8AHLxK4GcAb8UYPTH4QLKQ'
+);
 
 export default function Home() {
+  const [buyRecord, setBuyRecord] = useState([]);
+
+  useEffect(() => {
+    getBuyRecord();
+  }, []);
+
+  async function getBuyRecord() {
+    const { data } = await supabase.from('buy_record').select();
+    console.log(data);
+  }
+
   return (
     <main className="">
       <div className="">
