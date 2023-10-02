2344d6e868a8fece3401248a6a2ccb7656bbedf4
jenfang <jenfang91325@gmail.com>
2023-10-02 14:19:08 +0800

    commmmmt

--- app/goodtea/TeaColorTest.js
+++ app/goodtea/TeaColorTest.js
@@ -100,7 +100,7 @@ const TeaColorTest = () => {
     formData.append('teaType', selectedTea);
     try {
       const response = await axios.post(
-        'https://c7f1-140-119-19-30.ngrok-free.app/upload',
+        'https://7a8f-140-119-19-30.ngrok-free.app/upload',
         formData,
         {
           headers: {
