4c71f8eaddf4fa37fb8b67abb9654bf39c88b660
Mattis3858 <109306011@nccu.edu.tw>
2023-10-18 20:18:58 +0800

    更新env file解決vercel問題

--- app/api/auth/[...nextauth]/supabase.ts
+++ app/api/auth/[...nextauth]/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URI = process.env.SUPABASE_URI;
-const SUPABASE_SECRET = process.env.SUPABASE_SECRET;
+const SUPABASE_URI = process.env.NEXT_PUBLIC_SUPABASE_URI;
+const SUPABASE_SECRET = process.env.NEXT_PUBLIC_SUPABASE_SECRET;
 
 export const supabase = createClient(SUPABASE_URI, SUPABASE_SECRET);
